349af8211f7c324570aff11e6839c308c32a59fb
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-04-11 20:50:13 -0400

    feat: add cartTile

---
+++ loja_virtual_8/lib/tiles/cart_tile.dart
@@ -0,0 +1,93 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:loja_virtual_8/datas/cart_product.dart';
+import 'package:loja_virtual_8/datas/product_data.dart';
+
+class CartTile extends StatelessWidget {
+
+  final CartProduct cartProduct;
+
+  CartTile(this.cartProduct);
+
+  Widget _buildContent(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(8.0),
+          width: 120.0,
+          child: Image.network(cartProduct.productData.images[0], fit: BoxFit.cover,),          
+        ),
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  cartProduct.productData.title,
+                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 17.0),
+                ),
+                Text(
+                  "Tamanho: ${cartProduct.size}",
+                  style: TextStyle(fontWeight: FontWeight.w300),
+                ),
+                Text(
+                  "R\$ ${cartProduct.productData.price.toStringAsFixed(2)}",
+                  style: TextStyle(color: Theme.of(context).primaryColor, fontWeight: FontWeight.bold),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.remove, color: Theme.of(context).primaryColor,),
+                      onPressed: cartProduct.quantity > 1 ? () {} : null, 
+                    ),
+                    Text(cartProduct.quantity.toString()),
+                    IconButton(
+                      icon: Icon(Icons.add, color: Theme.of(context).primaryColor),
+                      onPressed: () {},
+                    ),
+                    FlatButton(
+                      child: Text("remover"),
+                      textColor: Colors.grey[500],
+                      onPressed: () {
+
+                      },
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+      child: cartProduct.productData == null ? 
+        FutureBuilder<DocumentSnapshot>(
+          future: Firestore.instance.collection("products").document(cartProduct.category)
+                  .collection("items").document(cartProduct.pid).get(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              cartProduct.productData = ProductData.fromDocument(snapshot.data);
+              return _buildContent(context);
+            } else {
+              return Container(
+                height: 70.0,
+                child: CircularProgressIndicator(),
+                alignment: Alignment.center,
+              );
+            }
+          },
+        )
+      : _buildContent(context)
+    );
+  }
+}
