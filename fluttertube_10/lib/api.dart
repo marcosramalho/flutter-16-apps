dfa2b1f0db77432d4c9f7155b41c0d7f48da3932
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-06-16 00:17:02 -0400

    feat: add search videos

--- fluttertube_10/lib/api.dart
+++ fluttertube_10/lib/api.dart
@@ -1,1 +1,34 @@
-const API_KEY = "AIzaSyAwCdziFhiYqg0qORc2ySe-xNY8tlGIs60";
+import 'dart:convert';
+
+import 'package:fluttertube_10/models/video.dart';
+import 'package:fluttertube_10/screens/home.dart';
+import 'package:http/http.dart' as http;
+
+const API_KEY = "AIzaSyDL2RzzhlJ0VH8_xeOFWKLjZGxUWyJPC5c";
+
+class Api {
+
+  search(String search) async {
+
+    http.Response response = await http.get(
+      "https://www.googleapis.com/youtube/v3/search?part=snippet&q=$search&type=video&key=$API_KEY&maxResults=10"
+    );
+
+    decode(response);
+  }
+
+  List<Video> decode(http.Response response) {
+
+    if (response.statusCode == 200) {
+      var decoded = json.decode(response.body);
+
+      List<Video> videos = decoded["items"].map<Video>((map) {
+        return Video.fromJson(map);
+      }).toList();
+
+      return videos;
+    }
+
+    throw Exception("Failed to load videos");
+  }
+}
--- fluttertube_10/lib/main.dart
+++ fluttertube_10/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertube_10/api.dart';
 import 'package:fluttertube_10/screens/home.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  Api api = Api();
+
+  await api.search("eletro");
+  runApp(MyApp());
+} 
+
 
 class MyApp extends StatelessWidget {
   @override
