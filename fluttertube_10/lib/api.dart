3ca42acfbca4c238cbb4a6c5d0546c704acb552f
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-06-15 23:37:30 -0400

    feat: add new project fluttertube and init config

---
+++ fluttertube_10/lib/api.dart
@@ -0,0 +1,1 @@
+const API_KEY = "AIzaSyAwCdziFhiYqg0qORc2ySe-xNY8tlGIs60";
---
+++ fluttertube_10/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertube_10/screens/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Home(),
+    );
+  }
+}
