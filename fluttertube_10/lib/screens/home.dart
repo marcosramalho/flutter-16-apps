d4a3295d5b81b52cd36f4fc5f96fe8d56c38bd2e
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-06-16 00:32:39 -0400

    feat: add icons button screen home

--- fluttertube_10/lib/screens/home.dart
+++ fluttertube_10/lib/screens/home.dart
@@ -3,8 +3,36 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 25,
+          child: Image.asset("images/yt_logo_rgb_dark.png"),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black87,
+        actions: <Widget>[
+          Align(
+            alignment: Alignment.center,
+            child: Text("0"),
+          ),
+          IconButton(
+            icon: Icon(Icons.star), 
+            onPressed: () {
+
+            }
+          ),
+          IconButton(
+            icon: Icon(Icons.search), 
+            onPressed: () {
+
+            }
+          )
+        ],
+      ),
+      body: Container(
+        
+      ),
     );
   }
 }
