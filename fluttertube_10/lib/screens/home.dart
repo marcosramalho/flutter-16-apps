6f9a2bdf31cf641f454677df959b5deb48264279
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-06-29 22:45:34 -0400

    feat: add infinite list

--- fluttertube_10/lib/screens/home.dart
+++ fluttertube_10/lib/screens/home.dart
@@ -40,13 +40,28 @@ class Home extends StatelessWidget {
       ),
       body: StreamBuilder(
         stream: BlocProvider.getBloc<VideosBloc>().outVideos,
+        initialData: [],
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             return ListView.builder(
               itemBuilder: (context, index) {
-                return VideoTile(snapshot.data[index]);
+                if (index < snapshot.data.length)
+                  return VideoTile(snapshot.data[index]);
+                
+                if (index > 1) {
+                  BlocProvider.getBloc<VideosBloc>().inSearch.add(null);
+                  return Container(
+                    height: 40,
+                    width: 40,
+                    alignment: Alignment.center,
+                    child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Colors.red),),
+                  );
+                }
+
+                return Container();
+                
               },
-              itemCount: snapshot.data.length,
+              itemCount: snapshot.data.length + 1,
             );
           }
 
