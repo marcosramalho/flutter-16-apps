2e05e489d5263fa52e16c32afea195bc542a61e0
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-07-21 22:22:42 -0400

    feat: add basic animations

--- animations_11/lib/main.dart
+++ animations_11/lib/main.dart
@@ -9,7 +9,56 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Animations Intro',
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,   
+      home: LogoApp(),   
+    );
+  }
+}
+
+class LogoApp extends StatefulWidget {
+  @override
+  _LogoAppState createState() => _LogoAppState();
+}
+
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+
+  AnimationController controller;
+  Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+
+    );
+
+    animation = Tween<double>(begin: 0, end:  300).animate(controller);
+    animation.addListener(() {
+      setState(() {
+        
+      });
+    });
+
+    controller.forward();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        height: animation.value,
+        width: animation.value,
+        child: FlutterLogo(),
+      ),
     );
   }
 }
