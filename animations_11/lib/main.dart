b44827481cef9cb1f446803b17f4f77d441dcc28
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-07-21 22:39:49 -0400

    feat: add animation2

--- animations_11/lib/main.dart
+++ animations_11/lib/main.dart
@@ -24,6 +24,7 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
 
   AnimationController controller;
   Animation<double> animation;
+  Animation<double> animation2;
 
   @override
   void initState() {
@@ -36,10 +37,21 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
     );
 
     animation = Tween<double>(begin: 0, end:  300).animate(controller);
-    animation.addListener(() {
-      setState(() {
-        
-      });
+    animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed)  {
+        controller.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        controller.forward();
+      }
+    });
+
+    animation2 = Tween<double>(begin: 0, end:  150).animate(controller);
+    animation2.addStatusListener((status) {
+      if (status == AnimationStatus.completed)  {
+        controller.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        controller.forward();
+      }
     });
 
     controller.forward();
@@ -53,11 +65,66 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        GrowTransition(
+          child: LogoWidget(),
+          animation: animation,
+        ),
+        GrowTransition(
+          child: LogoWidget(),
+          animation: animation2,
+        )
+      ],
+    );
+  }
+}
+
+/*class AnimatedLogo extends AnimatedWidget {
+
+  AnimatedLogo(Animation<double> animation): super(listenable: animation);
+
+  @override
+  Widget build(BuildContext context) {
+    final Animation<double> animation = listenable;
+
     return Center(
       child: Container(
         height: animation.value,
         width: animation.value,
         child: FlutterLogo(),
+      ),
+    );
+  }
+}*/
+
+class LogoWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FlutterLogo();
+  }
+}
+
+class GrowTransition extends StatelessWidget {
+
+  final Widget child;
+  final Animation<double> animation;
+
+  GrowTransition({this.child, this.animation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: AnimatedBuilder(
+        animation: animation, 
+        builder: (context, child) {
+          return Container(
+            height: animation.value,
+            width: animation.value,
+            child: child,
+          );
+        },
+        child: child,
       ),
     );
   }
