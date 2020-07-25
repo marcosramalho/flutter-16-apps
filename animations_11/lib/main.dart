0aaf880d107e21e782e1301c331e37c2634aa3ee
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-07-25 18:34:30 -0400

    feat: add loginScreen and animation button

--- animations_11/lib/main.dart
+++ animations_11/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:animations_11/screens/login/login_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,111 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Animations Intro',
       debugShowCheckedModeBanner: false,   
-      home: LogoApp(),   
-    );
-  }
-}
-
-class LogoApp extends StatefulWidget {
-  @override
-  _LogoAppState createState() => _LogoAppState();
-}
-
-class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
-
-  AnimationController controller;
-  Animation<double> animation;  
-
-  @override
-  void initState() {
-    super.initState();
-
-    controller = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 2),
-
-    );
-
-    animation = CurvedAnimation(parent: controller, curve: Curves.elasticOut);
-    animation.addStatusListener((status) {
-      if (status == AnimationStatus.completed)  {
-        controller.reverse();
-      } else if (status == AnimationStatus.dismissed) {
-        controller.forward();
-      }
-    });
-
-    controller.forward();
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: MarcosTransition(
-        child: LogoWidget(),
-        animation: animation,
-      ),
-    );
-  }
-}
-
-/*class AnimatedLogo extends AnimatedWidget {
-
-  AnimatedLogo(Animation<double> animation): super(listenable: animation);
-
-  @override
-  Widget build(BuildContext context) {
-    final Animation<double> animation = listenable;
-
-    return Center(
-      child: Container(
-        height: animation.value,
-        width: animation.value,
-        child: FlutterLogo(),
-      ),
-    );
-  }
-}*/
-
-class LogoWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return FlutterLogo();
-  }
-}
-
-class MarcosTransition extends StatelessWidget {
-
-  final Widget child;
-  final Animation<double> animation;
-  final sizeTween = Tween<double>(begin: 0, end: 300);
-  final opacityTween = Tween<double>(begin: 0.1, end: 1);
-
-  MarcosTransition({this.child, this.animation});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: AnimatedBuilder(
-        animation: animation, 
-        builder: (context, child) {
-          return Opacity(
-            opacity: opacityTween.evaluate(animation).clamp(0, 1.0),
-            child: Container(
-              height: sizeTween.evaluate(animation),
-              width: sizeTween.evaluate(animation),
-              child: child,
-            ),
-          );
-        },
-        child: child,
-      ),
+      home: LoginScreen(),   
     );
   }
 }
